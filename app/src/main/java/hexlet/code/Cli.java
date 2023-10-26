f4891569b66c583728657b8d7aa9dca031bb9891
e.dudin <e.dudin@qiwi.com>
2023-10-27 00:06:02 +0300

    init

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greeting(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("May I have your name?");
+        final String username = scan.next();
+        final String greetings = String.format("Hello, %s!", username);
+        System.out.println(greetings);
+
+        scan.close();
+    }
+}
