4349e57614eb20802f133fa8d59f5902a8154ec1
e.dudin <e.dudin@qiwi.com>
2023-11-01 05:39:45 +0300

    smth strange but ok

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("Welcome to the Brain Games!\nPlease enter the game number and press Enter.");
         System.out.println("\n1 - Greet\n2 - Even\n3 - Calc\n4 - Nod\n0 - Exit");
         System.out.print("Your choice: ");
 
