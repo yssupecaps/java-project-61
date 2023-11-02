c6ded50ba66034d550298334950d5ebb7e149c94
e.dudin <e.dudin@qiwi.com>
2023-11-02 04:28:09 +0300

    fix bug w '9'

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -2,35 +2,27 @@ package hexlet.code.games;
 
 import hexlet.code.Game;
 
+import java.math.BigInteger;
+
 import static hexlet.code.Engine.getRandomValue;
 
 public final class Prime implements Game {
 
     public static final int MIN_RAND_NUM = 0;
-    public static final int MAX_RAND_NUM = 20;
+    public static final int MAX_RAND_NUM = 100;
 
     @Override
     public String gameRules() {
         return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     }
 
-    public static String isPrime(int random) {
-        if (random <= 1) {
-            return "no";
-        }
-        for (int i = 2; i < Math.sqrt(random); i++) {
-            if (random % i == 0) {
-                return "no";
-            }
-        }
-        return "yes";
-    }
 
     @Override
     public String[] questionAndAnswers() {
         String[] gameData = new String[2];
         int randValue = getRandomValue(MIN_RAND_NUM, MAX_RAND_NUM);
-        String correctAnswer = isPrime(randValue);
+        BigInteger bi = BigInteger.valueOf(randValue);
+        String correctAnswer = bi.isProbablePrime(randValue) ? "yes" : "no";
         gameData[0] = correctAnswer;
         gameData[1] = String.valueOf(randValue);
         return gameData;
